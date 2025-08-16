f61b1033aa66e7d0360f47c2719f2180bc3910b1
Mahnoor Fatima <aminadreamy568@gmail.com>
2025-08-16 09:51:42 +0500

    Create Valid Palindrome.py

---
+++ Easy /Valid Palindrome.py
@@ -0,0 +1,31 @@
+class solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
+                sb.append(c);
+            }
+        }
+
+        int left = 0;
+        int right = sb.length() - 1;
+
+        while (left <= right) {
+            if (sb.charAt(left) != sb.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
+
+Time Complexity = O(n)
+Space Complexity = O(n)
+        
